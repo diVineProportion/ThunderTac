067000bf59311add1a418faf00a1aa531965d79b
diVineProportion <38984875+diVineProportion@users.noreply.github.com>
2019-12-30 23:40:16 -0800

    Update getpywin32.py

--- getpywin32.py
+++ getpywin32.py
@@ -1,3 +1,16 @@
+import sys
 import wget
-url = 'https://github.com/mhammond/pywin32/releases/download/b227/pywin32-227.win-amd64-py3.7.exe'
+
+major = sys.version_info[0]
+minor = sys.version_info[1]
+arch = sys.version[-7:-2]
+
+if arch == "AMD64":
+    arch = "win-amd64"
+else:
+    arch = "win32"
+
+url = "https://github.com/mhammond/pywin32/releases/download/b227/pywin32-227.{arch}-py{maj}.{min}.exe".format(
+    arch=arch, maj=major, min=minor)
+
 filename = wget.download(url)
