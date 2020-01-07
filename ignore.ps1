0eb33a17af6252fe4953831d8c2663133594ce94
diVineProportion <38984875+diVineProportion@users.noreply.github.com>
2020-01-07 02:35:35 -0800

    Add files via upload
    completed automatic build process

---
+++ ignore.ps1
@@ -0,0 +1,17 @@
+﻿if ([Environment]::Is64BitOperatingSystem -eq $True) {$url = "https://www.python.org/ftp/python/3.7.4/python-3.7.4-amd64.exe"}
+else {$url = "https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe"}
+
+
+$file = $url.Split("/")[-1]
+
+$path = "$env:TEMP\$file"
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile($url, $path)
+
+Start-Process -FilePath $path -ArgumentList "/quiet InstallLauncherAllUsers=0 PrependPath=1 Include_doc=0 Include_test=0 Include_tcltk=0" -Wait
+
+
+
+
+
