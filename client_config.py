8717e5114244b4ef9252664a209fcff6468e5957
divine <thedivineproportions@gmail.com>
2020-12-18 06:06:09 -0800

    init commit for cross platform version

--- client_config.py
+++
@@ -1,8 +0,0 @@
-class ClientConfig(object):
-    PUBLIC_KEY = 'gp4xou4FKWdJGoiMHaXH5EMSG7qafZtPqCnLJ0DRxZU'
-    APP_NAME = 'ThunderTac'
-    APP_VERSION = '0.0.15.1.0'
-    COMPANY_NAME = 'WarThunderApps'
-    HTTP_TIMEOUT = 30
-    MAX_DOWNLOAD_RETRIES = 3
-    UPDATE_URLS = ['https://update.tacserv.tk/files/']
